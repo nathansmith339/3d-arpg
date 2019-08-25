da25b130e745fb2a083d0027fedb0247afb0c4e5
Angry Pasta <theangriestpasta@gmail.com>
2019-08-25 01:13:27 -0400

    Camera Cleaned Up
    Cleaned all the unussed variables.

--- Target.gd
+++ Target.gd
@@ -2,23 +2,21 @@ extends Spatial
 
 const MAX_LOOK_UP_DEG = -30
 const MAX_LOOK_DOWN_DEG = 30
-const MAX_LOOK_LEFT_DEG = 45
-const MAX_LOOK_RIGHT_DEG = -45
 
 var look_speed = 0.02
 
 func _ready():
-    set_physics_process(true)
-    set_as_toplevel(true)
+	set_physics_process(true)
+	set_as_toplevel(false)
 
 func _physics_process(delta):
-    var hor_dir = (int(Input.is_action_pressed("ui_right")) * -1) + int(Input.is_action_pressed("ui_left"))
-    var ver_dir = (int(Input.is_action_pressed("ui_down")) * -1) + int(Input.is_action_pressed("ui_up"))
-
-    if ver_dir != 0 || hor_dir != 0:
-        rotate_object_local(Vector3(-1, 0, 0), look_speed * ver_dir * PI) # rotate in Y first
-        rotate_object_local(Vector3(0, -1, 0), look_speed * hor_dir * PI) # then rotate in X
-
-        rotation_degrees.x = clamp(rotation_degrees.x, MAX_LOOK_UP_DEG, MAX_LOOK_DOWN_DEG)
-        rotation_degrees.y = rotation_degrees.y
-        rotation_degrees.z = 0
+	var hor_dir = (int(Input.is_action_pressed("ui_right")) * -1) + int(Input.is_action_pressed("ui_left"))
+	var ver_dir = (int(Input.is_action_pressed("ui_down")) * -1) + int(Input.is_action_pressed("ui_up"))
+		
+	if ver_dir != 0 || hor_dir != 0:
+		rotate_object_local(Vector3(-1, 0, 0), look_speed * ver_dir * PI) # rotate in Y first
+		rotate_object_local(Vector3(0, -1, 0), look_speed * hor_dir * PI) # then rotate in X
+		
+		rotation_degrees.x = clamp(rotation_degrees.x, MAX_LOOK_UP_DEG, MAX_LOOK_DOWN_DEG)
+		rotation_degrees.y = rotation_degrees.y
+		rotation_degrees.z = 0

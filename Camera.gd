15bff3f6b8a453c096eb58bd31a5b609267e2934
Angry Pasta <theangriestpasta@gmail.com>
2019-08-24 23:24:06 -0400

    camera
    Rewrote camera code for y axis.

--- Camera.gd
+++ Camera.gd
@@ -1,37 +1,27 @@
 extends Camera
 
-var distance = 4
-var fixed = false
-var frozen = false
+var height = 0
 var target
-var outter
-var inner
+var vel = Vector3()
+var offset
+var distance = 5
 const UP = Vector3(0, 1, 0) 
 
 func _ready():
-	target = get_parent().get_parent().get_parent()
-	outter = get_parent().get_parent()
-	inner = get_parent()
-	
-	outter.set_physics_process(true)
-	outter.set_as_toplevel(true)
+	set_physics_process(true)
+	set_as_toplevel(true)
+	target = get_parent()
 	
 func _physics_process(delta):
 	var hor_dir = (int(Input.is_action_pressed("ui_left")) * -1) + int(Input.is_action_pressed("ui_right"))
 	var ver_dir = (int(Input.is_action_pressed("ui_up")) * -1) + int(Input.is_action_pressed("ui_down"))
 	if hor_dir != 0 || ver_dir != 0:
-		outter.set_as_toplevel(false)
-		target.rotate_y(hor_dir * .01)
-		inner.rotate_x(ver_dir * .01)
-	else:
-		outter.set_as_toplevel(true)
-	
+		height = clamp(height + ver_dir * .01, -.9, 1)
+		pass
 	var target_pos = target.get_global_transform().origin
-	var outter_pos = outter.get_global_transform().origin
-	var offset = outter_pos - target_pos
-	
-	offset = offset.normalized()
-	outter_pos = target_pos + offset
-	
-	outter.look_at_from_position(outter_pos, target_pos, UP)
-	
+	var self_pos = get_global_transform().origin
+	offset = self_pos - target_pos
+	offset.y = height
+	offset = offset.normalized() * distance
+	self_pos = target_pos + offset
+	look_at_from_position(self_pos, target_pos, UP)
